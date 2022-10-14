fea66787e02792117224f11d86d01be5d9a0fae3
Damilare Oyediran <43407276+ddoyediran@users.noreply.github.com>
2022-10-14 19:13:44 +0200

    Update week-4.js

--- week-4.js
+++ week-4.js
@@ -164,3 +164,56 @@ blogPost["comments"].commentTwo = {
 console.log(blogPost)
 
 
+/// Exercise 5 ///
+
+
+let programming = {
+    languages: ["JavaScript", "Python", "Ruby"],
+    isChallenging: true,
+    isRewarding: true,
+    difficulty: 8,
+    jokes:"http://stackoverflow.com/questions/234075/what-is-your-best-programmer-joke"
+};
+
+// Write the command to add the language "Go" to the end of the languages array.
+programming["languages"].push("Go")
+console.log(programming["languages"][3]) // expected output: "Go"
+
+
+// Change the difficulty to the value of 7.
+programming["difficulty"] = 7
+console.log(programming["difficulty"]) // expected output: 7
+
+// Using the delete keyword, write the command to remove the jokes key from the programming object.
+delete programming["jokes"] 
+console.log(programming["jokes"]) // expected output: undefined
+
+// Write the command to add a new key called isFun and a value of true to the programming object.
+programming["isFun"] = true
+console.log(programming["isFun"]) // expected output: true
+
+
+// Using a loop, iterate through the languages array and console.log all of the languages.
+for(let prop in programming){
+  if(prop === "languages"){
+    for(let i = 0; i < programming[prop].length; i++){
+      console.log(programming[prop][i])
+    }
+  }
+}
+
+// Another way:
+for(let i = 0; i < programming["languages"].length; i++){
+      console.log(programming["languages"][i])
+}
+
+// Using a loop, console.log all of the keys in the programming object.
+for(let prop in programming){
+  console.log(prop)
+}
+
+
+// Using a loop, console.log all of the values in the programming object
+for(let prop in programming){
+  console.log(programming[prop])
+}
